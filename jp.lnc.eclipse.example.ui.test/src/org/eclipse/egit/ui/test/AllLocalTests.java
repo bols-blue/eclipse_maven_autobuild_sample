e9e184827f253a02a60cdf74a9f81b50fbccd381
bols <bols-blue@lnc.jp>
2011-05-16 16:26:44 +0900

    commit files

--- jp.lnc.eclipse.example.ui.test/src/org/eclipse/egit/ui/test/AllLocalTests.java
+++ jp.lnc.eclipse.example.ui.test/src/org/eclipse/egit/ui/test/AllLocalTests.java
@@ -8,35 +8,13 @@
  *******************************************************************************/
 package org.eclipse.egit.ui.test;
 
-import org.eclipse.egit.ui.dialogs.commit.SpellcheckableMessageAreaTest;
-import org.eclipse.egit.ui.httpauth.PushTest;
-import org.eclipse.egit.ui.prefpages.configuration.GlobalConfigurationPageTest;
-import org.eclipse.egit.ui.test.history.HistoryViewTest;
-import org.eclipse.egit.ui.test.team.actions.AllTeamActionTests;
-import org.eclipse.egit.ui.test.trace.TraceConfigurationDialogTest;
-import org.eclipse.egit.ui.view.repositories.AllRepositoriesViewTests;
-import org.eclipse.egit.ui.view.synchronize.SynchronizeViewGitChangeSetModelTest;
-import org.eclipse.egit.ui.view.synchronize.SynchronizeViewWorkspaceModelTest;
-import org.eclipse.egit.ui.wizards.clone.GitCloneWizardHttpTest;
-import org.eclipse.egit.ui.wizards.clone.GitCloneWizardTest;
-import org.eclipse.egit.ui.wizards.share.SharingWizardTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses( { AllRepositoriesViewTests.class, 	//
-		GlobalConfigurationPageTest.class,			//
-		GitCloneWizardTest.class,                   //
-		GitCloneWizardHttpTest.class,               //
-		SharingWizardTest.class,					//
-		AllTeamActionTests.class,                   //
-		HistoryViewTest.class,                      //
-		PushTest.class,
-		SpellcheckableMessageAreaTest.class,
-		TraceConfigurationDialogTest.class,
-		SynchronizeViewWorkspaceModelTest.class,
-		SynchronizeViewGitChangeSetModelTest.class })
+@SuiteClasses( { //AllRepositoriesViewTests.class, 	//
+		 })
 public class AllLocalTests {
 	// empty class, don't need anything here
 }
