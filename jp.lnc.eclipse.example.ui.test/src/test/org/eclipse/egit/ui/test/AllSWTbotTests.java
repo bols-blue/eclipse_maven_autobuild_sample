d977d21312023f9a4fb804f51a3b0f9a190477f2
bols <bols-blue@lnc.jp>
2011-05-17 11:08:52 +0900

    commit files

---
+++ jp.lnc.eclipse.example.ui.test/src/test/org/eclipse/egit/ui/test/AllSWTbotTests.java
@@ -0,0 +1,16 @@
+package org.eclipse.egit.ui.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllSWTbotTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllSWTbotTests.class.getName());
+		//$JUnit-BEGIN$
+
+		//$JUnit-END$
+		return suite;
+	}
+
+}
