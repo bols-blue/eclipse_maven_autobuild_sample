5f24f3525f521a3d921547308adf9257059d8008
bols <bols-blue@lnc.jp>
2011-05-17 11:08:27 +0900

    commit sample files

--- jp.lnc.eclipse.example.ui.test/src/test/org/eclipse/egit/ui/test/AllLocalTests.java
+++ jp.lnc.eclipse.example.ui.test/src/test/org/eclipse/egit/ui/test/AllLocalTests.java
@@ -14,7 +14,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses( { //AllRepositoriesViewTests.class, 	//
-		 Eclipse.class})
+	AllSWTbotTests.class})
 public class AllLocalTests {
 	// empty class, don't need anything here
 }
